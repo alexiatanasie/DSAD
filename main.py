569beb955f5da03b29712f284ceb7674a657c297
Alexia <150076398+alexiatanasie@users.noreply.github.com>
2024-12-03 00:31:51 +0200

    Update

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ matrix[3, 3] = 77
 
 for i in range(7):
     matrix[i, 0] = 7
-    matrix[0,0]=33
     matrix[0, i] = 7
     matrix[i, 6] = 7
     matrix[6, i] = 7
-    matrix[6,6]=33
+    matrix[0, 0] = 33
+    matrix[6, 6] = 33
 
 print(matrix)
-
 #2
 #initialize the numpy.ndarray array
 nan1=np.full((7,7),np.nan)
@@ -26,18 +25,15 @@ print(matrix1)
 print("\n")
 
 #3 from matrix ex 2 obtain a sub-matrix containing the cells with val = 0
-# Task 3
-matrix2 = matrix1 == 0.0
-submatrix = np.where(matrix2, 0.0, 5.0)
-print(submatrix)
-print("\n")
-
+submatrix= matrix1[1:6, 1:6]
+print("Submatricea cu valorile 0:\n", submatrix)
 #4
 #generate a vector of 100 random floating-point values in the range [0,10). create a pandas.Series based on this vector and label its rows as 'L_1','L_2'. print to the console
 vector=np.random.uniform(0,10,100)
 label=['L_'+str(i+1) for i in range(100) ]
 series=pd.Series(vector,index=label)
 print(series)
+print("\n")
 
 #5
 # create a 2-dimensional numpy.ndarray(11,5) of random floating-point values in [0,10). obtain a pandas.DateFrame from this array with row labels of the form 'L1','L2' and column lables of 'C1','C2'. print DateFrame to the console
@@ -62,23 +58,23 @@ df7=pd.DataFrame(dict2)
 print(df7)
 print("\n")
 
-#the following have some warnings  waaaa
-#dar afiseaza ce tb oricum ;))
 
 #8
-#read tbe files Series1_csv adn Series2_csv and bring the date into 2 pandas.Series. create a python dictionary with the keys 'Col1','Col2' and having as values the 2 Series with the data taken from the files. create a pandas.DF from the previuosly cerated dictionary and print it to console.
-'''
-series1 = pd.read_csv('./dataIN/Series_1.csv', index_col=0)
-series2 = pd.read_csv('./dataIN/Series_2.csv', index_col=0)
+#read tbe files Series1_csv adn Series2_csv and bring the date into 2 pandas.Series. create a python dictionary with the keys 'Col1','Col2' and having as values the 2 Series with the data taken from the files. create a pandas.DF from the previuosly created dictionary and print it to console.
+series1 = pd.read_csv('./dataIN/Series_1.csv', index_col=0).iloc[:, 0]
+series2 = pd.read_csv('./dataIN/Series_2.csv', index_col=0).iloc[:, 0]
+
 print("Series 1:")
 print(series1)
 print("\nSeries 2:")
 print(series2)
 
+series1, series2 = series1.align(series2, join='outer')
 merged_dict = {"Col1": series1, "Col2": series2}
 merged_df = pd.DataFrame(merged_dict)
-
 print(merged_df)
+print("\n")
+
 
 #9
 #create a dictionary where the keys are years of study of the form 'An1',..'An5' and the values are dictionaries with keys of the form 'Stud1'...'Stud8' anf the values are 5 random grades from 1 to 10. create a pandas.DF from this dictionary of dictionaries and print it to cons.
@@ -89,9 +85,13 @@ dict4 = {
     }
     for i in range(5)
 }
-for (k, v) in dict4.items():
-    print(k, ":", v)
+df_list = []
+for year, students in dict4.items():
+    year_df = pd.DataFrame(students)
+    year_df.index = ['Ex' +str(k+1) for k in range(5)]
+    year_df['An'] = year
+    df_list.append(year_df)
 
-df9 = pd.DataFrame.from_dict({k: pd.DataFrame(v) for k, v in dict4.items()})
-print(df9)
-'''
+final_df = pd.concat(df_list)
+
+print(final_df)
