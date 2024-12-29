15750a90547c3df0a65847d1d66200c84820d9fa
Alexia <150076398+alexiatanasie@users.noreply.github.com>
2024-12-29 19:46:34 +0200

    Delete Seminar 8 directory

--- Seminar 8/functions.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def standardize(X):
-
-    return None
--- Seminar 8/mainPCA.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import functions as fun
-
-
-table = pd.read_csv('./dataIN/Teritorial.csv', index_col=0)
-print(table)
-
