9dc828c9a687a28493d1580673603b357447150b
Alexia <150076398+alexiatanasie@users.noreply.github.com>
2024-12-29 19:46:12 +0200

    Delete Seminar 5 directory

--- Seminar 5/Seminar5.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import graphics5 as graph
-#generate a matrix of(7,5) random integer values in [1,10]
-nda1=np.random.randint(1,10+1,size=(7,5))
-print(nda1,type(nda1))
-
-#create a pandas.DataFrame from a numpy.ndarray
